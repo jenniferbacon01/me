329162eafad9d1bbe0d9cdada7986bf2ad1fe2d7
Jenny Bacon <jenniferbacon01@gmail.com>
2017-10-15 09:35:57 +0100

    aboutme, projects, interests and contact populated with basic info

---
+++ app.js
@@ -0,0 +1,27 @@
+var express = require('express');
+
+var app = express();
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
+
+app.set('view engine', 'ejs');
+
+app.use( express.static( "public" ) );
+
+app.get('/', function (req, res) {
+  res.render('aboutme');
+})
+
+app.get('/projects', function (req, res) {
+  res.render('projects');
+})
+
+app.get('/interests', function (req, res) {
+  res.render('interests');
+})
+
+app.get('/contact', function (req, res) {
+  res.render('contact');
+})
