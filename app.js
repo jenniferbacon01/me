cad0808b6f5c97aac3f40883d5dbff086f4fd315
Jenny Bacon <jenniferbacon01@gmail.com>
2017-10-15 11:29:40 +0100

    reordered app config with public folder first

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ var express = require('express');
 
 var app = express();
 
+app.use( express.static( "public" ) );
+
 app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening on port 3000!')
 })
 
-app.use( express.static( "public" ) );
+
+
 
 app.set('view engine', 'ejs');
 
